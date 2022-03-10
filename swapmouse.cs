9f978bb61cac6e18029d101877e8521fac263c34
Guoyuan Zhao <zhaoguoyuan@gmail.com>
2022-03-10 10:41:13 +0900

    add source code

---
+++ swapmouse.cs
@@ -0,0 +1,17 @@
+using System.Runtime.InteropServices;
+using System;
+
+class SwapMouse
+{
+    [DllImport("user32.dll")]
+    public static extern Int32 SwapMouseButton(Int32 bSwap);
+
+    static void Main(string[] args)
+    {
+        int rightButtonIsAlreadyPrimary = SwapMouseButton(1);
+        if (rightButtonIsAlreadyPrimary != 0)
+        {
+            SwapMouseButton(0);  // Make the left mousebutton primary
+        }
+    }
+}
